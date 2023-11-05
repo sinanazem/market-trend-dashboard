d6a9b23328e0502139ff081899ad6617abb08a7d
sinanazem <sinanaazem@gmail.com>
2023-11-06 00:32:21 +0330

    move wordcloud subpackage

--- src/wordcloud_smt/wordcloud_generator.py
+++
@@ -1,9 +0,0 @@
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-
-
-def wc_generator(content):
-    wordcloud_obj = WordCloud(background_color='white')
-    wordcloud_obj.generate(content)
-    return wordcloud_obj
-
