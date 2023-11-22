fabcfe7cec54ac3c9ff323bb09bfe4be9f30d934
Sina Nazem <92341812+sinanazem@users.noreply.github.com>
2023-11-22 23:47:45 +0330

    Update transform.py

--- src/transform.py
+++ src/transform.py
@@ -1,2 +1,11 @@
+import pandas as pd
+
+
+def transform_data(file_path):
+    df = pd.DataFrame(file_path)
+    df.drop_duplicates(inplace=True)
+    return df
+    
+
 def main():
-    pass
+    transform_data()
